9c4772c10381b88d509bae9750b5260bdb20dd3b
KevinRuiz <kevin_ruiz_sanchez@hotmail.com>
2025-01-15 07:12:53 -0500

    Eliminacion de Archivos de la plantilla

--- backdssl/src/repositories/userRepository.js
+++
@@ -1,9 +0,0 @@
-const CrudRepository = require('../lib/crudRepository');
-const User = require('../models/User');
-
-class UserRepository extends CrudRepository {
-    constructor(){
-        super(User);
-    }
-}
-module.exports = new UserRepository();
